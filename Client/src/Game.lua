62acaf17dbe2fed8de829e7af01fce39a7341ae9
Noe Tuluz <47043735+DoktaTuluz@users.noreply.github.com>
2019-04-01 22:19:33 +0200

    NetGame - Walbach
    Again an update, that makes the client only connect the server when accessing the "real" game. There is a new connection waiting scene between the menu, and the game play scene.

--- Client/src/Game.lua
+++ Client/src/Game.lua
@@ -3,11 +3,14 @@ require("SceneManager")
 require("UserImplementedScenes")
 require("Sprite")
 
+local sock = require("SocketConnection")
+
 local Game = {}
 Game.SceneManager = SceneManager("SceneManager")
 
 function Game:init()
     Game.SceneManager:addScene(SceneMenu())
+    Game.SceneManager:addScene(SceneConnection(sock))
     Game.SceneManager:addScene(SceneGameplay())
     Game.SceneManager:playScene("menu")
 end
@@ -23,7 +26,19 @@ function Game:draw()
 end
 
 
+    -- Exit the program
+local function exitProgram()
+    if sock.sock ~= nil then
+        sock:send(0, "DISCONNECTION", "Cut the bound with the server "..sock:getPeerServer())
+    end
+    os.exit()
+end
+
+
 function Game:keypressed(key)
+    if key == "escape" then
+        exitProgram()
+    end
 end
 
 return Game
--- Client/src/main.lua
+++ Client/src/main.lua
@@ -10,7 +10,6 @@ db.Print("+---------------------------------------------------------------+")
 db.Print("|            NetGame - Walbach (c) Noé Toulouze 2019            |")
 db.Print("+---------------------------------------------------------------+\n")
 
-local sock = require("SocketConnection")
 local mainGame = require("Game")
 
 
@@ -19,49 +18,20 @@ function love.load()
     love.window.setTitle("NetGame")
     love.window.setMode(800, 600, { fullscreen=false })
 
-    -- Server setup
-    sock:init()
-
     mainGame:init()
 end
 
 
 function love.update(dt)
-    for e in love.event.poll() do
-        if e == "quit" then
-            exitProgram()
-        end
-    end
-
-    sock:checkSocket()
-    sock:update(dt)
     mainGame:update(dt)
 end
 
 
 function love.draw()
-    if sock.connected == false then
-        love.graphics.setColor(1, 0, 0, 1)
-        love.graphics.print("WAITING FOR AN AVAILABLE SERVER", 100, 100)
-        love.graphics.setColor(1, 1, 1, 1)
-        return
-    end
-
     mainGame:draw()
 end
 
 
-    -- Exit the program
-local function exitProgram()
-    sock:send(0, "DISCONNECTION", "Disconnect from the server")
-    os.exit()
-end
-
-
 function love.keypressed(key)
-    if key == "escape" then
-        exitProgram()
-    end
-
     mainGame:keypressed(key)
 end
